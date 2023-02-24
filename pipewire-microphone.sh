4af3c7b7dbb8e90b9e468210da150cd5207ecfcd
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-02-24 21:25:36 +0545

    migrate pactl to wpctl

--- pipewire-microphone.sh
+++ pipewire-microphone.sh
@@ -1,58 +1,43 @@
 #!/bin/sh
 
- status() {
-   MUTED=$(pactl list sources  | grep Mute | tail -n1 | cut -d: -f2   | tr -d ' ')
+status() {
+	echo $(wpctl status | grep Sources -A2 | grep vol | sed s/[' '\]/\\n/g | tail -n1 | tr -d ] | cut -d. -f2)%
+}
 
-  if [ "$MUTED" = "yes" ]; then
-     echo "muted"
-   else
-     pactl list sources | grep Volume | tail -n2 | head -n1 |  cut -d'/' -f2 | tr -d ' ' 
-   fi
- }
+listen() {
+	status
 
- listen() {
-     status
+	LANG=EN
+	pactl subscribe | while read -r event; do
+		if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
+			status
+		fi
+	done
+}
 
-     LANG=EN; pactl subscribe | while read -r event; do
-         if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
-             status
-         fi
-     done
- }
+toggle() {
+	wpctl set-mute $(wpctl status | grep Sources -A2 | grep vol | cut -d. -f1 | tr -d [\│\*\ ]) toggle
+}
 
- toggle() {
-   MUTED=$(pactl list sources  | grep Mute | tail -n1 | cut -d: -f2   | tr -d ' ')
-  DEFAULT_SOURCE=$(pactl list sources  | grep Name | tail -n1 | cut -d':' -f2 | tr -d ' ')
+increase() {
+	wpctl set-volume $(wpctl status | grep Sources -A2 | grep vol | cut -d. -f1 | tr -d [\│\*\ ]) 1%+
+}
 
-   if [ "$MUTED" = "yes" ]; then
-       pactl set-source-mute "$DEFAULT_SOURCE" 0
-   else
-       pactl set-source-mute "$DEFAULT_SOURCE" 1
-   fi
- }
+decrease() {
+	wpctl set-volume $(wpctl status | grep Sources -A2 | grep vol | cut -d. -f1 | tr -d [\│\*\ ]) 1%-
+}
 
- increase() {
-   DEFAULT_SOURCE=$(pactl list sources  | grep Name | tail -n1 | cut -d':' -f2 | tr -d ' ')
-   pactl set-source-volume "$DEFAULT_SOURCE" +5%
- }
-
- decrease() {
-   DEFAULT_SOURCE=$(pactl list sources  | grep Name | tail -n1 | cut -d':' -f2 | tr -d ' ')
-   pactl set-source-volume "$DEFAULT_SOURCE" -5%
- }
-
- case "$1" in
-     --toggle)
-         toggle
-         ;;
-     --increase)
-         increase
-         ;;
-     --decrease)
-         decrease
-         ;;
-     *)
-         listen
-         ;;
- esac
-
+case "$1" in
+--toggle)
+	toggle
+	;;
+--increase)
+	increase
+	;;
+--decrease)
+	decrease
+	;;
+*)
+	listen
+	;;
+esac
